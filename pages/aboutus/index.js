c615c88f73eef23ed48528268c5421d88866742f
IthaManisha <manisha.itha2001@gmail.com>
2023-08-27 20:24:21 +0530

    final touch to aboutus page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,6 +1,23 @@
-
+import Link from "next/link";
 function AboutusPage(){
-    return <h1>The Aboutus Page</h1>
+    const details = [
+        { id: 1, name: 'Yash', role: 'Senior Developer' },
+        { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+        { id: 3, name: 'Suresh', role: 'Frontend Developer' },
+      ];
+      
+    return (
+        <div>
+        <h1>The Aboutus Page</h1>
+        <ul>
+        {details.map(detail => (
+          <li key={detail.id}>
+            <Link href={`/aboutus/${detail.id}`}>{detail.name}</Link>
+          </li>
+        ))}
+      </ul>
+        </div>
+    )
 }
 
 export default AboutusPage
